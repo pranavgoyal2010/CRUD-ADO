a0a746a89c90793acb66b776109e6035b99c27f2
Pranav Goyal <pr945005@dal.ca>
2024-02-28 15:27:16 +0530

    [Pranav] Add: code added to connect with database.

---
+++ Runner.cs
@@ -0,0 +1,37 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace EmployeeADO;
+
+class Runner
+{
+    static void Main(string[] args)
+    {
+        string connectionString = "Data Source=DESKTOP-9TNLIB5\\SQLEXPRESS;Initial Catalog=ado_db;Integrated Security=true;";
+
+        //establishing connection
+        Connection(connectionString);
+    }
+
+    static void Connection(string connectionString)
+    {
+        SqlConnection conn = null;
+
+        try
+        {
+            using (conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                if (conn.State == ConnectionState.Open)
+                {
+                    Console.WriteLine("Connection with database established");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
